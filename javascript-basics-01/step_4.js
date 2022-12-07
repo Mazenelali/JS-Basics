9187fdbd35cde8cedb964eac88017651878fe06f
Mazen El Ali <mazenelali>
2022-12-07 15:04:48 +0200

    completed step-4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,6 @@
+let person = prompt("what ur name")
+let username = prompt("what ur username")
+let city = prompt("what ur city")
+alert("hello "+person+"\n"+"ur username is: "+username+"\n"+"ur city is: "+city)
+ 
+
