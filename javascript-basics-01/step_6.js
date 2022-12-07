36c4c90da0b7024cfaf63d437079a90eddb62d89
Mazen El Ali <mazenelali>
2022-12-07 15:57:46 +0200

    completed step-6

---
+++ javascript-basics-01/step_6.js
@@ -0,0 +1,6 @@
+function divise(){
+    let a = document.getElementById("first_number").value
+    let b = document.getElementById("second_number").value
+    let x = a/b 
+    alert("divide to 2 number is: " + x)
+}
