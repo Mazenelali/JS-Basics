4d9087d2b0396d2181d335d355584d02301b45ce
Mazen El Ali <mazenelali>
2022-12-07 16:19:55 +0200

    completed step-7

---
+++ javascript-basics-01/step_7.js
@@ -0,0 +1,7 @@
+function functon1() {
+    let a = document.getElementById("shoe_size").value
+    let b = document.getElementById("year").value
+    let x = (a*2)+(5*50)-b+1766;
+    alert("the result is: "+ x)
+
+}
