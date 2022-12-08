c9ee67298dc97127740161006a522eaa058a9407
Mazen El Ali <mazenelali>
2022-12-08 13:03:34 +0200

    completed step4

--- javascript-basics-02/step 1/step1.js
+++ javascript-basics-02/step 1/step1.js
@@ -8,4 +8,4 @@ document.getElementById("image1").onmouseover=function() {newimage()}
 function lastimage(){
     document.getElementById("image1").src="images/image1.jpg"
 }
-document.getElementById("image1").onmouseout=function() {lastimage()}
+document.getElementById("image1").onmouseout=function() {newimage()}
--- javascript-basics-02/step 4/step4.js
+++ javascript-basics-02/step 4/step4.js
@@ -1,1 +1,10 @@
- 
+
+function reset(){
+let a = confirm("you want to reset ?")
+if (a==true){
+  document.getElementById("name").value=" ";
+  document.getElementById("surname").value=" ";
+  document.getElementById("city").value=" ";
+}
+}
+document.querySelector("button").onclick=function(){reset()}
