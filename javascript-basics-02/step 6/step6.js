8f6a12408f00e88e08014ae06f6e5e9125d58fa9
Mazen El Ali <mazenelali>
2022-12-08 15:02:02 +0200

    completed step6

---
+++ javascript-basics-02/step 6/step6.js
@@ -0,0 +1,11 @@
+var image = document.querySelectorAll("img")
+ 
+for ( let i=0 ; i<=image.length; i++){
+    image[i].addEventListener("mouseover",function(){
+        document.getElementById("image"+(i+1)).src="images/image"+(i+1)+"_2.jpg"
+    })
+
+        image[i].addEventListener("mouseleave",function(){
+            document.getElementById("image"+(i+1)).src="images/image"+(i+1)+".jpg"
+        })
+    }
